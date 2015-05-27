3e7148a1948b399b6d730fff2fad8557a8b09a9d
Nass <nasali@anypresence.com>
2015-05-27 06:50:49 -0600

    Debug

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -2,24 +2,24 @@ install_ap_binary_dependencies() {
   if test -f $1/.oracle.ini; then
     install_oci8
   else
-    echo "No trigger found."
+    info "No trigger found."
   fi
 }
 
 install_oci8() {
-  echo "Installing Oracle OCI8 dependencies"
+  info "Installing Oracle OCI8 dependencies"
 
   s3bucket="https://s3.amazonaws.com/chameleon-heroku-assets"
   oracle_instant_client_tgz="$s3bucket/instantclient_11_2_with_libaio_oci8.tar.gz"
   oracle_instant_client_dir="$1/vendor/oracle_instantclient"
 
-  echo "Creating directory"
+  info "Creating directory"
   mkdir -p $oracle_instant_client_dir
 
-  echo "Downloading and installing Oracle binaries from $s3bucket"
+  info "Downloading and installing Oracle binaries from $s3bucket"
   curl $oracle_instant_client_tgz -s -o - | tar xzf - -C $oracle_instant_client_dir
 
-  echo "Dir is now $oracle_instant_client_dir"
+  info "Dir is now $oracle_instant_client_dir"
 
   LD_LIBRARY_PATH=$oracle_instant_client_dir:$LD_LIBRARY_PATH
   OCI_LIB_DIR=$oracle_instant_client_dir
