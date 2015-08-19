ade9f0e6fe660036f1a95b041a9c5267e0656388
Nass <nasali@anypresence.com>
2015-08-19 10:00:09 -0600

    Refactor.

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -7,26 +7,31 @@ install_ap_binary_dependencies() {
 }
 
 install_oci8() {
-  info "Installing Oracle OCI8 dependencies"
+  echo "Installing Oracle OCI8 dependencies"
 
   local s3bucket="https://s3.amazonaws.com/chameleon-heroku-assets"
   local oracle_instant_client_tgz="$s3bucket/instantclient_11_2_with_libaio_oci8.tar.gz"
   local oracle_instant_client_dir="$1/.heroku/oracle_instantclient"
 
-  info "Creating directory"
+  echo "Creating directory"
   mkdir -p $oracle_instant_client_dir
 
-  info "Downloading and installing Oracle binaries from $s3bucket"
+  echo "Downloading and installing Oracle binaries from $s3bucket"
   curl $oracle_instant_client_tgz -s -o - | tar xzf - -C $oracle_instant_client_dir
 
   local oci_inc="${oracle_instant_client_dir}/sdk/include"
   mkdir -p $1/.profile.d
-  echo "export LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}:${oracle_instant_client_dir}\"" > $1/.profile.d/nodejs.sh
-  echo "export LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}:${oracle_instant_client_dir}\"" >> $2/export
+  echo "export LD_LIBRARY_PATH=\"\$HOME/vendor/oracle_instantclient:\$LD_LIBRARY_PATH\"" > $1/.profile.d/nodejs.sh
+  echo "export OCI_LIB_DIR=\"\$HOME/vendor/oracle_instantclient\"" >> $1/.profile.d/nodejs.sh
+  echo "export OCI_INC_DIR=\"\$HOME/vendor/oracle_instantclient/sdk/include\"" >> $1/.profile.d/nodejs.sh
 
-  export OCI_INC_DIR="${oci_inc}"
-  export OCI_LIB_DIR="${oracle_instant_client_dir}"
-  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:${oracle_instant_client_dir}"
+  echo "export OCI_INC_DIR=\"${oci_inc}\"" > $2/export
+  echo "export OCI_LIB_DIR=\"${oracle_instant_client_dir}\"" >> $2/export
+  echo "export LD_LIBRARY_PATH=\"${oracle_instant_client_dir}\"" >> $2/export
 
-  info "Done installing OCI8."
+  export OCI_INC_DIR=${oci_inc}
+  export OCI_LIB_DIR=${oracle_instant_client_dir}
+  export LD_LIBRARY_PATH=${oracle_instant_client_dir}:${LD_LIBRARY_PATH:-}
+
+  echo "Done installing OCI8."
 }
