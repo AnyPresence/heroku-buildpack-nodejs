030cfc0a5c3508dbcdeea056f50f03b241fc11e6
Nass <nasali@anypresence.com>
2015-08-19 10:58:08 -0600

    Append instead.

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -22,11 +22,11 @@ install_oci8() {
   local oci_inc="${oracle_instant_client_dir}/sdk/include"
 
   mkdir -p $1/.profile.d
-  echo "export LD_LIBRARY_PATH=\"\${HOME}/.heroku/oracle_instantclient:\$LD_LIBRARY_PATH\"" > $1/.profile.d/nodejs.sh
+  echo "export LD_LIBRARY_PATH=\"\${HOME}/.heroku/oracle_instantclient:\$LD_LIBRARY_PATH\"" >> $1/.profile.d/nodejs.sh
   echo "export OCI_LIB_DIR=\"\${HOME}/.heroku/oracle_instantclient\"" >> $1/.profile.d/nodejs.sh
   echo "export OCI_INC_DIR=\"\${HOME}/.heroku/oracle_instantclient/sdk/include\"" >> $1/.profile.d/nodejs.sh
 
-  echo "export OCI_INC_DIR=\"${oci_inc}\"" > $2/export
+  echo "export OCI_INC_DIR=\"${oci_inc}\"" >> $2/export
   echo "export OCI_LIB_DIR=\"${oracle_instant_client_dir}\"" >> $2/export
   echo "export LD_LIBRARY_PATH=\"${oracle_instant_client_dir}:\${LD_LIBRARY_PATH:-}\"" >> $2/export
 
