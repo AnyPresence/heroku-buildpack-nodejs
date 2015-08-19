40cfdc4d92a66c51b025284217c69f37f8a3baca
Nass <nasali@anypresence.com>
2015-08-19 10:32:14 -0600

    Test using ldconfig instead.

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -19,19 +19,9 @@ install_oci8() {
   echo "Downloading and installing Oracle binaries from $s3bucket"
   curl $oracle_instant_client_tgz -s -o - | tar xzf - -C $oracle_instant_client_dir
 
-  local oci_inc="${oracle_instant_client_dir}/sdk/include"
-  mkdir -p $1/.profile.d
-  echo "export LD_LIBRARY_PATH=\"\${HOME}/vendor/oracle_instantclient:\$LD_LIBRARY_PATH\"" > $1/.profile.d/nodejs.sh
-  echo "export OCI_LIB_DIR=\"\${HOME}/vendor/oracle_instantclient\"" >> $1/.profile.d/nodejs.sh
-  echo "export OCI_INC_DIR=\"\${HOME}/vendor/oracle_instantclient/sdk/include\"" >> $1/.profile.d/nodejs.sh
+  echo "${oracle_instant_client_dir}" > "/etc/ld.so.conf.d/oracle-instantclient.conf"
 
-  echo "export OCI_INC_DIR=\"${oci_inc}\"" > $2/export
-  echo "export OCI_LIB_DIR=\"${oracle_instant_client_dir}\"" >> $2/export
-  echo "export LD_LIBRARY_PATH=\"${oracle_instant_client_dir}:\${LD_LIBRARY_PATH:-}\"" >> $2/export
-
-  export OCI_INC_DIR=${oci_inc}
-  export OCI_LIB_DIR=${oracle_instant_client_dir}
-  export LD_LIBRARY_PATH=${oracle_instant_client_dir}:${LD_LIBRARY_PATH:-}
+  sudo ldconfig
 
   echo "Done installing OCI8."
 }
