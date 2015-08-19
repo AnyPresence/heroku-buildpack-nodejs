9f85b03afaff4c0d9f4c1f1f13a252c678507876
Nass <nasali@anypresence.com>
2015-08-19 10:12:53 -0600

    Please work.

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -20,21 +20,21 @@ install_oci8() {
   curl $oracle_instant_client_tgz -s -o - | tar xzf - -C $oracle_instant_client_dir
 
   local oci_inc="${oracle_instant_client_dir}/sdk/include"
-  local profile_dir = "$1/profile"
-  mkdir -p "$profile_dir"
-  local profile_file = "$profile_dir/oci.sh"
+  local profile_dir = "${1}/profile"
+  mkdir -p "${profile_dir}"
+  local profile_file = "${profile_dir}/oci.sh"
   echo "export LD_LIBRARY_PATH=\"\$HOME/vendor/oracle_instantclient:\$LD_LIBRARY_PATH\"" > $profile_file
   echo "export OCI_LIB_DIR=\"\$HOME/vendor/oracle_instantclient\"" >> $profile_file
   echo "export OCI_INC_DIR=\"\$HOME/vendor/oracle_instantclient/sdk/include\"" >> $profile_file
 
-  local export_file = "$2/export"
+  local export_file = "${2}/export"
   echo "export OCI_INC_DIR=\"${oci_inc}\"" > $export_file
   echo "export OCI_LIB_DIR=\"${oracle_instant_client_dir}\"" >> $export_file
   echo "export LD_LIBRARY_PATH=\"${oracle_instant_client_dir}\"" >> $export_file
 
-  export OCI_INC_DIR=${oci_inc}
-  export OCI_LIB_DIR=${oracle_instant_client_dir}
-  export LD_LIBRARY_PATH=${oracle_instant_client_dir}:${LD_LIBRARY_PATH:-}
+  export OCI_INC_DIR="${oci_inc}"
+  export OCI_LIB_DIR="${oracle_instant_client_dir}"
+  export LD_LIBRARY_PATH="${oracle_instant_client_dir}:${LD_LIBRARY_PATH:-}"
 
   echo "Done installing OCI8."
 }
