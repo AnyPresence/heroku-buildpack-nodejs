66e009724fcd1c5e23226266055272d00569f204
Nass <nasali@anypresence.com>
2015-08-19 09:53:38 -0600

    Try OCI env var again.

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -11,7 +11,7 @@ install_oci8() {
 
   local s3bucket="https://s3.amazonaws.com/chameleon-heroku-assets"
   local oracle_instant_client_tgz="$s3bucket/instantclient_11_2_with_libaio_oci8.tar.gz"
-  local oracle_instant_client_dir="$1/vendor/oracle_instantclient"
+  local oracle_instant_client_dir="$1/.heroku/oracle_instantclient"
 
   info "Creating directory"
   mkdir -p $oracle_instant_client_dir
@@ -19,10 +19,10 @@ install_oci8() {
   info "Downloading and installing Oracle binaries from $s3bucket"
   curl $oracle_instant_client_tgz -s -o - | tar xzf - -C $oracle_instant_client_dir
 
-  local oci_inc="$oracle_instant_client_dir/sdk/include"
+  local oci_inc="${oracle_instant_client_dir}/sdk/include"
   mkdir -p $1/.profile.d
-  echo "export LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}\${HOME}/vendor/oracle_instantclient\"" > $1/.profile.d/nodejs.sh
-  echo "export LD_LIBRARY_PATH=\"\$oracle_instant_client_dir\"" >> $2/export
+  echo "export LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}:${oracle_instant_client_dir}\"" > $1/.profile.d/nodejs.sh
+  echo "export LD_LIBRARY_PATH=\"\${LD_LIBRARY_PATH}:${oracle_instant_client_dir}\"" >> $2/export
 
   export OCI_INC_DIR="${oci_inc}"
   export OCI_LIB_DIR="${oracle_instant_client_dir}"
