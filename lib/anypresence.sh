3cc250118f714f18e69e3314e0952da2c0e37c3e
Nass <nasali@anypresence.com>
2015-08-19 10:11:09 -0600

    Refactor.

--- lib/anypresence.sh
+++ lib/anypresence.sh
@@ -11,7 +11,7 @@ install_oci8() {
 
   local s3bucket="https://s3.amazonaws.com/chameleon-heroku-assets"
   local oracle_instant_client_tgz="$s3bucket/instantclient_11_2_with_libaio_oci8.tar.gz"
-  local oracle_instant_client_dir="$1/.heroku/oracle_instantclient"
+  local oracle_instant_client_dir="$1/vendor/oracle_instantclient"
 
   echo "Creating directory"
   mkdir -p $oracle_instant_client_dir
@@ -20,14 +20,17 @@ install_oci8() {
   curl $oracle_instant_client_tgz -s -o - | tar xzf - -C $oracle_instant_client_dir
 
   local oci_inc="${oracle_instant_client_dir}/sdk/include"
-  mkdir -p $1/.profile.d
-  echo "export LD_LIBRARY_PATH=\"\$HOME/vendor/oracle_instantclient:\$LD_LIBRARY_PATH\"" > $1/.profile.d/nodejs.sh
-  echo "export OCI_LIB_DIR=\"\$HOME/vendor/oracle_instantclient\"" >> $1/.profile.d/nodejs.sh
-  echo "export OCI_INC_DIR=\"\$HOME/vendor/oracle_instantclient/sdk/include\"" >> $1/.profile.d/nodejs.sh
+  local profile_dir = "$1/profile"
+  mkdir -p "$profile_dir"
+  local profile_file = "$profile_dir/oci.sh"
+  echo "export LD_LIBRARY_PATH=\"\$HOME/vendor/oracle_instantclient:\$LD_LIBRARY_PATH\"" > $profile_file
+  echo "export OCI_LIB_DIR=\"\$HOME/vendor/oracle_instantclient\"" >> $profile_file
+  echo "export OCI_INC_DIR=\"\$HOME/vendor/oracle_instantclient/sdk/include\"" >> $profile_file
 
-  echo "export OCI_INC_DIR=\"${oci_inc}\"" > $2/export
-  echo "export OCI_LIB_DIR=\"${oracle_instant_client_dir}\"" >> $2/export
-  echo "export LD_LIBRARY_PATH=\"${oracle_instant_client_dir}\"" >> $2/export
+  local export_file = "$2/export"
+  echo "export OCI_INC_DIR=\"${oci_inc}\"" > $export_file
+  echo "export OCI_LIB_DIR=\"${oracle_instant_client_dir}\"" >> $export_file
+  echo "export LD_LIBRARY_PATH=\"${oracle_instant_client_dir}\"" >> $export_file
 
   export OCI_INC_DIR=${oci_inc}
   export OCI_LIB_DIR=${oracle_instant_client_dir}
